a0d4c7ef6000896e40c60d4ed97ad962ffcf27b1
omarnefzi <omarnefzi001@gmail.com>
2023-06-21 15:43:36 +0100

    third commit

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
-const jsonServer = require('json-server');
+const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3001; // you can use any port number here; i chose to use 3001
 
 server.use(middlewares);
-server.use('/api', router);
+server.use(router);
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
-});
+server.listen(port);
